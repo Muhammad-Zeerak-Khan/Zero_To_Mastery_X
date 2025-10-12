4acba1bb26ff02d02143895a3f5831ef24ee4f65
Muhammad Zeerak Khan <zeerak1994@outlook.com>
2025-10-12 16:05:23 +0200

    Env creation and setup

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from zero-to-mastery-x!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ ztm_pytorch/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import torch
+
+print(torch.__version__)
+print(np.__version__)
+print(pd.__version__)
